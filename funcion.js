5d439958149d15030f4069794ac1c2b95d31e5c6
nparrac1 <npcruz630@gmail.com>
2023-08-31 11:45:42 -0400

    Clase-6-js

---
+++ funcion.js
@@ -0,0 +1,23 @@
+/* console.log("Hola Mundo");
+
+$(document).ready(function(){
+    alert("Hello, World!");
+}); */
+
+/* function hola(){
+    alert("Hola mundo en js");
+} */
+
+function verde(){
+    document.getElementsByClassName("verde")[0].style.backgroundColor ="green";
+}
+
+function azul(){
+    document.getElementById("azul").style.backgroundColor="blue";
+    
+}
+
+function reset(){
+    document.getElementById("azul").style.backgroundColor="red";
+    
+}
