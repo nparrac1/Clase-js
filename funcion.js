b8435182bb020c6c6c7e1266ffa97cdab59c53a4
nparrac1 <npcruz630@gmail.com>
2023-09-01 11:46:10 -0400

    Clase-7

--- funcion.js
+++ funcion.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     alert("Hola mundo en js");
 } */
 
-function verde(){
+/* function verde(){
     document.getElementsByClassName("verde")[0].style.backgroundColor ="green";
 }
 
@@ -20,4 +20,37 @@ function azul(){
 function reset(){
     document.getElementById("azul").style.backgroundColor="red";
     
+} */
+
+
+/* function girar(){
+    document.getElementsByClassName('verde')[0].style.animation = 'fading 4s infinite';
+    
+} */
+
+
+function sumar(){
+    var numero1 = $("#numero1").val();
+    var numero2 = $("#numero2").val();
+    var resultado = Number(numero1) + Number(numero2)
+    console.log("El resultado de la suma es: " + resultado)
+}
+
+function restar(){
+    var numero1 = $("#numero1").val();
+    var numero2 = $("#numero2").val();
+    var resultado = Number(numero1) - Number(numero2)
+    console.log("El resultado de la resta es: " + resultado)
+}
+function divi(){
+    var numero1 = $("#numero1").val();
+    var numero2 = $("#numero2").val();
+    var resultado = Number(numero1) / Number(numero2)
+    console.log("El resultado de la division es: " + resultado)
+}
+function multi(){
+    var numero1 = $("#numero1").val();
+    var numero2 = $("#numero2").val();
+    var resultado = Number(numero1) * Number(numero2)
+    console.log("El resultado de la multiplicacion es: " + resultado)
 }
